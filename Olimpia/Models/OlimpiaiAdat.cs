449cd798a4fde72078cca0dacf383a53acea2d68
Futty Imre <ap@d.dd>
2025-03-24 18:37:23 +0100

    Add project files.

---
+++ Olimpia/Models/OlimpiaiAdat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olimpia.Models
+{
+    public class OlimpiaiAdat
+    {
+        public int Ev { get; set; }
+        public string Sportag {  get; set; } = String.Empty;
+        public string? Nemzet {  get; set; }
+        public string Versenyzo {  get; set; }
+        public string Nem {  get; set; }
+
+        public OlimpiaiAdat() //üres konstruktor kell az adatbázis miatt
+        {
+        }
+
+        public OlimpiaiAdat(string sor)
+        {
+            var tomb = sor.Split(";");
+            Ev = Convert.ToInt32(tomb[0]);
+            Sportag = tomb[1];
+            Nemzet = tomb[2];
+            Versenyzo = tomb[3];
+            Nem = tomb[4];
+        }
+
+        public override string? ToString()
+        {
+            return $"{Ev} - {Versenyzo}, {Sportag}";
+        }
+    }
+}
---
+++ Olimpia/Program.cs
@@ -0,0 +1,81 @@
+﻿using Olimpia.Models;
+
+List<OlimpiaiAdat> adatok = new List<OlimpiaiAdat>();
+
+var sorok = File.ReadAllLines("olimpia.csv").Skip(1);
+foreach (var line in sorok)
+{
+    adatok.Add(new OlimpiaiAdat(line));
+}
+
+//foreach (var dij in adatok)
+//{
+//Console.WriteLine(dij);
+//}
+
+Console.WriteLine($"3. A listában {adatok.Count()} fő szerepel.");
+
+var uszok = adatok
+    .Where(x => x.Sportag.Equals("úszás"))
+    .OrderByDescending(x => x.Ev);
+
+
+Console.WriteLine("\n4. Úszók: ");
+
+foreach (var item in uszok)
+{
+    Console.WriteLine($"\t{item}");
+}
+
+var nok = adatok
+    .Where(x => x.Nem.Equals("Nő") && x.Ev == 2020);
+
+
+Console.WriteLine("\n6. Nők(2020):");
+
+foreach (var item in nok)
+{
+    Console.WriteLine($"\t{item}");
+}
+
+Console.WriteLine("\n7. Aranyérmek sportáganként:");
+
+//var arany = adatok
+    //.GroupBy(x => x.Sportag)
+    //.Select(x => new { x.Key, db = x.Count() })
+    //.OrderByDescending(x => x.db)
+    //.First();
+
+//Console.WriteLine($"\t{arany.Key} - {arany.db} db");
+
+var arany = adatok
+    .GroupBy(x => x.Sportag)
+    .OrderByDescending(x => x.Count())
+    .First();
+
+Console.WriteLine($"\t{arany.Key} - {arany.Count()} db");
+
+//foreach (var item in arany)
+//{
+//Console.WriteLine(item);
+//}
+
+var filebe = uszok
+    .Select(x => x.Ev + " - " + x.Versenyzo);
+
+//File.WriteAllLines("uszas.txt", filebe);
+
+//File.WriteAllLines(@"c:\adat\uszas.txt", filebe);
+
+var sportagSzam = adatok
+    .Select(x => x.Sportag)
+    .Distinct()
+    .Count();
+
+Console.WriteLine($"\nA sportágak száma: {sportagSzam}");
+
+var tobbarany = adatok
+    .GroupBy(x => x.Versenyzo)
+    .Select(x => new { x.Key, Db = x.Count() })
+    .Where(x => x.Db > 1);
+
